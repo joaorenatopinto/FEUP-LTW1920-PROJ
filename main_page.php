eae928d219b2dfb34e8dcf857a805144a5f87153
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-11-13 11:22:08 +0000

    navbar implemented

---
+++ main_page.php
@@ -0,0 +1,9 @@
+<?php 
+    include_once('templates/tlp_common.php');
+    draw_header();
+    draw_navbar();
+    draw_footer();
+?>
+
+
+
---
+++ templates/tlp_common.php
@@ -0,0 +1,37 @@
+<?php function draw_header() { ?>
+    <!DOCTYPE html>
+    <html lang="en-US">
+    <head>
+        <title>Houst.</title>
+        <meta charset="UTF-8">
+        <link href="css/navbar.css" rel="stylesheet">
+        <link href="css/common.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+<?php } ?>
+
+<!-- fazer aceitar $username como argumento para fazer navbar diferente para user -->
+<?php function draw_navbar(){ ?>
+    <header class="navbar">
+        <div class="logo_container">
+            <img class="logo" src="images/logo/logo.png" alt="logo">
+        </div>
+
+        <nav>
+            <ul class="nav_links">
+                <li><a href="#">Properties</a></li>
+                <li><a href="#">Messages</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">Profile</a></li>
+            </ul>
+        </nav>
+        <a class="cta" href="#"><button>List Property</button>
+    </header>
+<?php } ?>
+
+<?php function draw_footer() { ?>
+    </body>
+    </html>
+<?php } ?>
