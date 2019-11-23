9730d59e82b846a316687baebf8a36f5aa4c17b0
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-11-23 17:52:50 +0000

    log in page created

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+    include_once('templates/tlp_common.php');
+    include_once('templates/tlp_account.php');
+    draw_login_header();
+    draw_login();
+    draw_footer();
+?>
--- templates/tlp_account.php
+++ templates/tlp_account.php
@@ -2,9 +2,9 @@
 { ?>
     <div class="signup-card">
         <header>
-            <h3> Sign Up</h3>
+            <h3>Sign Up</h3>
         </header>
-        <form class="signup-form" method=post" action="#">
+        <form class="signup-form" method="post" action="#">
             <input id="sp-firstname" type="text" name="firstname" placeholder="First Name" required="">
             <input id="sp-lastname" type="text" name="lastname" placeholder="Last Name" required="">
             <input id="sp-username" type="text" name="username" placeholder="Username" required="">
@@ -12,6 +12,7 @@
             <input id="sp-confirmpassword" type="password" name="confirm-password" placeholder="Confirm Your Password" required="">
             <input id="sp-submit" type="submit" value="Sign Up">
         </form>
+        <p> Already have an account? Login <a href="#"> here! </a> </p>
     </div>
 <?php } ?>
 
@@ -27,4 +28,32 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
+<?php } ?>
+
+<?php function draw_login_header() { ?>
+    <!DOCTYPE html>
+    <html lang="en-US">
+    <head>
+        <title>Houst.</title>
+        <meta charset="UTF-8">
+        <link href="css/common.css" rel="stylesheet">
+        <link href="css/login.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600&display=swap" rel="stylesheet"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+<?php } ?>
+
+<?php function draw_login() { ?>
+    <div class="login-card">
+        <header>
+            <h3>Welcome back!</h3>
+        </header>
+        <form class="login-form" method="post" action="#">
+            <input id="li-username" type="text" name="username" placeholder="Username" required="">
+            <input id="li-password" type="password" name="password" placeholder="Password" required="">
+            <input id="li-submit" type="submit" value="Sign Up">
+        </form>
+        <p> Don't have an account? Sign up <a href="#">here! </a> </p>
+    </div>
 <?php } ?>
