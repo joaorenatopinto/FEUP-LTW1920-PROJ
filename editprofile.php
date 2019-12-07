2042344aae3625c1d8b7795e74c7f29f3f17f453
João Renato Pinto <jrcp210@gmail.com>
2019-12-07 23:00:04 +0000

    Improved edit profile page

--- editprofile.php
+++ editprofile.php
@@ -4,5 +4,7 @@
     include_once('templates/tlp_account.php');
     $user = get_user($_GET['username']);
     draw_edit_header();
+    draw_navbar();
+    draw_header();  
     draw_edit($user);
 ?>
